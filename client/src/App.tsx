01b936923a636d4e50fbe93fc0ac171277fb5c6c
jaffasunny <jaffer.sunny125@gmail.com>
2023-08-10 13:35:28 +0500

    Working on Form UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import InputTodo from "./components/InputTodo";
 
 function App() {
 	return (
-		<>
-			<h1>hello world</h1>
-		</>
+		<div className='container'>
+			<InputTodo />
+		</div>
 	);
 }
 
--- client/src/components/InputTodo.tsx
+++ client/src/components/InputTodo.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
 
-type Props = {}
+type Props = {};
 
 const InputTodo = (props: Props) => {
-  return (
-    <div>InputTodo</div>
-  )
-}
+	const [description, setDescription] = useState<string>("");
 
-export default InputTodo
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		try {
+			const body = { description };
+
+			const response = await fetch("http://localhost:5000/todos", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(body),
+			});
+
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<>
+			<h1 className='text-center mt-5'>Pern Todo List</h1>
+
+			<form className='d-flex mt-5' onSubmit={handleSubmit}>
+				<input
+					type='text'
+					className='form-control'
+					value={description}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+						setDescription(e.target.value)
+					}
+				/>
+
+				<button type='submit' className='btn btn-success'>
+					Add
+				</button>
+			</form>
+		</>
+	);
+};
+
+export default InputTodo;
